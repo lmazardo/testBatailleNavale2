8c8bfa7db62a33eb87570d04b200ed655a6880b5
Luc Mazardo <luc@mazardo.com>
2016-02-03 10:33:17 +0100

    more tests

--- src/main/java/BatailleNavale.java
+++ src/main/java/BatailleNavale.java
@@ -1,16 +1,55 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import static org.junit.Assert.fail;
+
 public class BatailleNavale {
 
     public boolean hasWin() {
         return hasWin;
     }
 
-    public void shoot() {
-        hasWin = true;
+    public void shoot(int pos) {
+        shoots.add(pos);
+        if(shoots.contains(3) && shoots.contains(5))
+            hasWin = true;
+        numTries++;
     }
 
-    public void shoot(int pos) {
-        hasWin = pos == 3 ;
+    public int getNumTries() {
+        return numTries;
     }
+
+    void run() {
+        while (!hasWin()) {
+            int pos = getInput();
+            shoot(pos);
+
+            if (MAX_TRY == getNumTries()) {
+                fail();
+            }
+            System.out.println("shoot");
+        }
+    }
+
+    private int getInput() {
+        Scanner scanner = new Scanner(System.in);
+        //return new Random().nextInt(4);
+        return scanner.nextInt();
+    }
+
+    public boolean hasLoose() {
+        if (numTries == MAX_TRY + 1) {
+            return true;
+        }
+        return false;
+    }
+
+
+    public static final int MAX_TRY = 10;
 
     private boolean hasWin;
+    private int numTries;
+
+    ArrayList shoots = new ArrayList<Integer>(100);
 }
--- src/test/java/BatailleNavaleTest.java
+++ src/test/java/BatailleNavaleTest.java
@@ -1,6 +1,6 @@
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class BatailleNavaleTest {
 
@@ -8,27 +8,50 @@ public class BatailleNavaleTest {
     public void couldCreateBatailleNavale() {
         BatailleNavale batailleNavale = new BatailleNavale();
 
+        assertEquals(false, batailleNavale.hasLoose());
         assertEquals(false, batailleNavale.hasWin());
+        assertEquals(0, batailleNavale.getNumTries());
 
     }
 
     @Test
-    public void shootAndwin() {
-        BatailleNavale batailleNavale = new BatailleNavale();
-
-        batailleNavale.shoot();
-
-        assertEquals(true, batailleNavale.hasWin());
-
-    }
-
-    @Test
-    public void shootIntTheBoat() {
+    public void shootInTheBoat() {
         BatailleNavale batailleNavale = new BatailleNavale();
 
         batailleNavale.shoot(3);
 
-        assertEquals(true, batailleNavale.hasWin());
+        assertEquals(false, batailleNavale.hasWin());
     }
 
+    @Test
+    public void whenShootingIncreasingNumTries() {
+        BatailleNavale batailleNavale = new BatailleNavale();
+
+        batailleNavale.shoot(0);
+
+        assertNotEquals(0, batailleNavale.getNumTries());
+
+    }
+
+    @Test
+    public void shootTenTimesInTheWater() {
+        BatailleNavale batailleNavale = new BatailleNavale();
+
+        for(int i=0; i < batailleNavale.MAX_TRY + 1 ; i++) {
+            batailleNavale.shoot(0);
+        }
+
+        assertEquals(true, batailleNavale.hasLoose());
+
+    }
+
+    @Test
+    public void shootTwoBoatsAndWin() {
+        BatailleNavale batailleNavale = new BatailleNavale();
+
+        batailleNavale.shoot(3);
+        batailleNavale.shoot(5);
+
+        assertEquals(true, batailleNavale.hasWin());
+    }
 }
